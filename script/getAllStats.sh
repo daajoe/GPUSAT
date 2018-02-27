ff0634a94a0050bc189f6eb7ea30785ba99fe893
Markus <markus.zisser@gmx.at>
2018-02-27 14:49:11 +0100

    cleanup

--- script/getAllStats.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python3 ./getStats.py
-python3 ./getModels.py
-./Summary.R
-Rscript -e "library(knitr); knit('Summary.Rmd')"
--- script/getAllStats_Cobra.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./beval ./runscripts/runscript-gpusat_cobra.xml > eval_cobra.xml
-./beval ./runscripts/runscript-gpusat_cobra.xml | ./bconv > result_cobra.ods
