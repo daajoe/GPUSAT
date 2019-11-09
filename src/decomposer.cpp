601c970c361a39b6fd9a210c7b7eb557a10d2100
Martin Röbke <martin.roebke@tu-dresden.de>
2019-11-09 18:57:17 +0100

    Replace Tabs with spaces
    Replace tabs with spaces in lines that are parsed to add conformity with DIMACS CNF format.
    Maybe additional characters could be supported - for now tabs added.

--- src/decomposer.cpp
+++ src/decomposer.cpp
@@ -18,6 +18,8 @@ namespace gpusat {
         while (getline(ss, item)) {
             //ignore empty line
             if (item.length() > 0) {
+                //replace tabs with spaces
+				std::replace(item.begin(), item.end(), '\t', ' ');
                 char type = item.at(0);
                 if (type == 'c' || type == '%') {
                     //comment line (ignore)
